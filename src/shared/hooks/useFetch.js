a6a052cd3be178f72ff320df770ddad54ed13d23
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-11-04 21:21:44 +0200

    Spinner styles changed

---
+++ src/shared/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = ({ initialState, fetchFunction, query = '' }) => {
+  const [state, setState] = useState({ ...initialState });
+
+  useEffect(() => {
+    const getItems = async () => {
+      setState(prevState => ({ ...prevState, loading: true }));
+      try {
+        const items = query
+          ? await fetchFunction(query)
+          : await fetchFunction();
+        setState(prevState => ({ ...prevState, items, loading: false }));
+      } catch (error) {
+        setState(prevState => ({ ...prevState, error, loading: false }));
+      }
+    };
+    getItems();
+  }, [query]);
+
+  return { state, setState };
+};
+
+export default useFetch;
